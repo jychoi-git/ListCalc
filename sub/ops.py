59776ac157886d803e7797cf403304ee1871d55a
jychoi-git <jychoinv@gmail.com>
2025-09-25 16:28:18 +0900

    ops.py 추가

---
+++ sub/ops.py
@@ -0,0 +1,11 @@
+def op(i, list):
+    if i == 0: # 차원(길이)
+        return list[0]
+    if i == 1: # 합
+        return list[0]
+    if i == 2: # 교대합
+        return list[0]
+    if i == 3: # 제곱합
+        return list[0]
+    if i == 4: # 벡터길이
+        return list[0]
